523970bd6f582d3861d1efe07881275efb0a7d09
Juan Quino <quinojuan@gmail.com>
2022-12-22 14:43:09 -0300

    Seccion controllers

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,11 +1,9 @@
 const { Router } = require("express");
-const pool = require("../db")
 
 const router = Router();
 
-router.get("/tasks", async (req, res) => {
-  const result = await pool.query("SELECT NOW()")
-  res.json(result.rows[0].now)
+router.get("/tasks", (req, res) => {
+  res.send("retrieving a list of task")
 });
 
 router.get("/tasks/10", (req, res) => {
