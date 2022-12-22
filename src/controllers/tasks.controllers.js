5d917d8fc8e7ca14e050b1154adc8e2c690486fe
Juan Quino <quinojuan@gmail.com>
2022-12-22 14:53:47 -0300

    controllers

---
+++ src/controllers/tasks.controllers.js
@@ -0,0 +1,26 @@
+const getAllTasks = async (req, res) => {
+  res.send("retrieven a list of task");
+};
+
+const getTask = (req, res) => {
+  res.send("Retrieving a single task");
+};
+
+const createTask = (req, res) => {
+  res.send("Creating a task");
+};
+
+const deleteTask = (req, res) => {
+  res.send("Deleting a task");
+};
+
+const updateTask = (req, res) => {
+  res.send("Updating a task");
+};
+module.exports = {
+  getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask,
+};
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,25 +1,22 @@
 const { Router } = require("express");
+const {
+  getAllTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask,
+} = require("../controllers/tasks.controllers");
 
 const router = Router();
 
-router.get("/tasks", (req, res) => {
-  res.send("retrieving a list of task")
-});
+router.get("/tasks", getAllTasks);
 
-router.get("/tasks/10", (req, res) => {
-  res.send("Retrieving a single task");
-});
+router.get("/tasks/10", getTask);
 
-router.post("/tasks", (req, res) => {
-  res.send("Creating a task");
-});
+router.post("/tasks", createTask);
 
-router.delete("/tasks", (req, res) => {
-  res.send("Deleting a task");
-});
+router.delete("/tasks", deleteTask);
 
-router.put("/tasks", (req, res) => {
-  res.send("Updating a task");
-});
+router.put("/tasks", updateTask);
 
 module.exports = router;
