f7c4cb988aa58798073d822a8f039baac42cad4c
Juan Quino <quinojuan@gmail.com>
2022-12-25 13:43:33 -0300

    post task ui

--- client/src/components/TaskForm.jsx
+++ client/src/components/TaskForm.jsx
@@ -5,8 +5,11 @@ import {
   TextField,
   Typography,
   Button,
+  CircularProgress,
 } from "@mui/material";
 import { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function TaskForm() {
   const [task, setTask] = useState({
@@ -14,9 +17,32 @@ export default function TaskForm() {
     description: "",
   });
 
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(task); // veo la tarea que finalmente voy a enviar al backend
+
+    //*******************CON FETCH*******************/
+
+    //   const res = await fetch("http://localhost:3001/tasks", {
+    //     method: "POST",
+    //     body: JSON.stringify(task), // convierte el objeto en un string
+    //     headers: { "Content-Type": "application/json" },
+    //   });
+    //   const data = await res.json();
+    //   console.log(data);
+
+    setLoading(true);
+    //*******************CON AXIOS*******************/
+
+    const res = await axios.post("http://localhost:3001/tasks", task);
+    const data = res.data;
+    console.log(data);
+
+    setLoading(false);
+    navigate("/");
   };
 
   const handleChange = (e) => {
@@ -68,8 +94,12 @@ export default function TaskForm() {
                 InputLabelProps={{ style: { color: "white" } }}
               />
 
-              <Button variant="contained" type="submit">
-                Save
+              <Button variant="contained" type="submit" disabled={!task.title || !task.description}> 
+                {loading ? (
+                  <CircularProgress color="inherit" size={24} />
+                ) : (
+                  "Create"
+                )}
               </Button>
             </form>
           </CardContent>
